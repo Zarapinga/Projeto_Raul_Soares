ef4864c0a5eac7326870f8d795ef516f1475f481
Raul Soares <raul.carvalho1@estudante.ufla.br>
2024-04-09 22:34:30 -0300

    Novo commit

---
+++ A.java
@@ -0,0 +1,35 @@
+public class A {
+
+    private int A1;
+    private float A2;
+
+    public A() {
+        A1 = 1;
+        A2 = 0.25;        
+    }
+
+    public void setA1(int a1) {
+        A1 = a1;
+    }
+
+    public void setA2(float a2) {
+        A2 = a2;
+    }
+
+    public int getA1(int a1) {
+        return A1;
+    }
+
+    public float getA2(float a2) {
+        return A2;
+    }
+
+    public void MA1(){
+        system.out.print("MA1")
+    }
+
+    public void MA2(){
+        system.out.print("MA2")
+    }
+    
+}
---
+++ B.java
@@ -0,0 +1,36 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public B {
+        B1 = 2;
+        B2 = 0.5;
+    }
+
+    public void setB1(int b1) {
+        B1 = b1;
+    }
+
+    public void setB2(float b2) {
+        B2 = b2;
+    }
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void MB1(){
+        system.out.print("MB1")
+    }
+
+    public void MB2(){
+        system.out.print("MB2")
+    }
+
+
+
+}
